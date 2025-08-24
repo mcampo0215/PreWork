3d6aa3856a9030c684066b797055e3441742aab9
mcampo0215 <matthewcampoverde@gmail.com>
2025-08-24 18:48:35 -0400

    Added proj.

---
+++ MyPreworkTests/MyPreworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPreworkTests.swift
+//  MyPreworkTests
+//
+//  Created by Matthew Campoverde on 8/24/25.
+//
+
+import Testing
+@testable import MyPrework
+
+struct MyPreworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
